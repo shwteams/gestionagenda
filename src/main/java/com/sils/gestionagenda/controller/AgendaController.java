29178d29f54122b816778672e0a4bf916b87cd03
shwteams <morokojeanr@hotmail.com>
2022-07-16 01:43:47 +0000

    ajout du planning et du diagramme des class. Ajout également de la vue agenda

--- src/main/java/com/sils/gestionagenda/controller/AgendaController.java
+++ src/main/java/com/sils/gestionagenda/controller/AgendaController.java
@@ -1,7 +1,13 @@
 package com.sils.gestionagenda.controller;
 
+import com.sils.gestionagenda.entities.Agenda;
 import com.sils.gestionagenda.repositories.AgendaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AgendaController {
@@ -9,6 +15,16 @@ public class AgendaController {
 
     public AgendaController(AgendaRepository agendaRepository) {
         this.agendaRepository = agendaRepository;
+    }
+
+    @GetMapping(path = "/user/agenda")
+    public String typesAgenda(Model model, @RequestParam(name="page", defaultValue = "0") int page, @RequestParam(name="size", defaultValue = "5") int size, @RequestParam(name="rechercher", defaultValue = "") String rechercher){
+        Page<Agenda> pageTypeAgenda = agendaRepository.findAgendaByTitreContains(rechercher, PageRequest.of(page, size));
+        model.addAttribute("listAgendas", pageTypeAgenda.getContent());
+        model.addAttribute("pages", new int[pageTypeAgenda.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("valeurRechercher", rechercher);
+        return "calendrier";
     }
 
 }
--- src/main/java/com/sils/gestionagenda/repositories/AgendaRepository.java
+++ src/main/java/com/sils/gestionagenda/repositories/AgendaRepository.java
@@ -1,7 +1,10 @@
 package com.sils.gestionagenda.repositories;
 
 import com.sils.gestionagenda.entities.Agenda;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+    Page<Agenda> findAgendaByTitreContains(String titre, Pageable pageable);
 }
